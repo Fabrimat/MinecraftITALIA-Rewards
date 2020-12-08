5cb5facb5c6fd16af95ed593dccdab2338978533
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2020-12-09 00:48:29 +0100

    Implemented announceManager

--- src/main/java/me/fabrimat/minecraftitaliarewards/announce/AnnounceRunner.java
+++ src/main/java/me/fabrimat/minecraftitaliarewards/announce/AnnounceRunner.java
@@ -1,7 +1,10 @@
 package me.fabrimat.minecraftitaliarewards.announce;
 
 import me.fabrimat.minecraftitaliarewards.MinecraftItaliaRewards;
+import me.fabrimat.minecraftitaliarewards.config.ConfigManager;
 import me.fabrimat.minecraftitaliarewards.database.DatabaseManager;
+import me.fabrimat.minecraftitaliarewards.message.MessageManager;
+import me.fabrimat.minecraftitaliarewards.sound.SoundManager;
 import me.fabrimat.minecraftitaliarewards.vote.VotesManager;
 
 import org.bukkit.Bukkit;
@@ -14,21 +17,29 @@ public class AnnounceRunner extends BukkitRunnable {
     private final MinecraftItaliaRewards plugin;
     private final VotesManager votesManager;
     private final DatabaseManager databaseManager;
+    private final MessageManager messageManager;
+    private final SoundManager soundManager;
+    private final ConfigManager configManager;
 
     public AnnounceRunner(MinecraftItaliaRewards plugin) {
         this.plugin = plugin;
         this.votesManager = plugin.getVotesManager();
         this.databaseManager = plugin.getDatabaseManager();
+        this.messageManager = plugin.getMessageManager();
+        this.soundManager = plugin.getSoundManager();
+        this.configManager = plugin.getConfigManager();
     }
 
     @Override
     public void run() {
         if(votesManager.isRewardDay()) {
             for(Player player : Bukkit.getOnlinePlayers()) {
-                if(player.hasPermission("") && !databaseManager.isPlayerInVotes(player.getUniqueId().toString())) {
+                if(player.hasPermission("mcita.reward") &&
+                        !databaseManager.isPlayerInVotes(player.getUniqueId().toString())) {
                     Bukkit.getScheduler().runTask(plugin, () -> {
-                        player.sendMessage(""); // TODO
-                        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 2.0F, 3.0F);
+                        messageManager.sendMessage(player, true,
+                                configManager.getMessageConfig().getString("claim-your-reward"));
+                        soundManager.playSound(player, Sound.BLOCK_NOTE_BLOCK_PLING, 2.0F, 3.0F);
                     });
                 }
             }
